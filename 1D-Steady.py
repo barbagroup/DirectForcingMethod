610452e9162d9adc63a98792cefe707f25a34623
Anush Krishnan <k.anush@gmail.com>
2014-08-12 10:02:26 -0400

    Edit 1D-Steady to work with fixed mesh and wall.

--- 1D-Steady.py
+++ 1D-Steady.py
@@ -8,72 +8,61 @@ import os
 
 nu = 0.125
 dpdx = -1.
+interp = 'constant'
 
-N = 5
+N = 6
 h = 1./N
 y = np.linspace(-0.5+h/2., 0.5-h/2., N)
-ratio = 0.5
-xi = ratio/(ratio+1.)
 width = 0.8
-left = -width/2.
-right = width/2.
+left = 0
+while y[left] < -width/2.:
+	left+=1
+xi_left = (y[left]+width/2.)/(y[left]+width/2.+h)
+right = N-1
+while y[right] > width/2.:
+	right-=1
+xi_right = (width/2.-y[right])/(width/2.-y[right]+h) 
+
 u = np.zeros(N)
 uExact = np.zeros(N)
 uExact[:] = dpdx/nu/8.*(4*y[:]*y[:]-width**2)
 
 # matrix
-rows = np.zeros(3*(N-2)+4, dtype=np.int)
-cols = np.zeros(3*(N-2)+4, dtype=np.int)
-vals = np.zeros(3*(N-2)+4, dtype=np.float)
+rows = np.zeros(3*N, dtype=np.int)
+cols = np.zeros(3*N, dtype=np.int)
+vals = np.zeros(3*N, dtype=np.float)
 # rhs
 b = np.zeros(N)
 
-row_index = 0
 index = 0
 
-rows[index] = row_index
-cols[index] = 0
-vals[index] = 1.
-index+=1
-
-rows[index] = row_index
-cols[index] = 1
-vals[index] = -xi
-index+=1
-
-row_index+=1
-
-for i in xrange(1,N-1):
-	rows[index] = row_index
-	cols[index] = i-1
-	vals[index] = 1.
+for i in xrange(N):
+	rows[index] = i
+	cols[index] = i-1 if i>0 else N-1
+	if i==left:
+		vals[index] = 0.
+	elif i==right:
+		vals[index] = 0. if interp=='constant' else -xi_right
+	else:
+		vals[index] = 1.
 	index+=1
 
-	rows[index] = row_index
+	rows[index] = i
 	cols[index] = i
-	vals[index] = -2.
+	vals[index] = 1. if (i==left or i==right) else -2.
 	index+=1
 
-	rows[index] = row_index
-	cols[index] = i+1
-	vals[index] = 1.
+	rows[index] = i
+	cols[index] = i+1 if i<N-1 else 0
+	if i==left:
+		vals[index] = 0. if interp=='constant' else -xi_left
+	elif i==right:
+		vals[index] = 0.
+	else:
+		vals[index] = 1.
 	index+=1
 
-	row_index+=1
-
-	b[i] = dpdx/nu*h**2
-
-rows[index] = row_index
-cols[index] = N-2
-vals[index] = -xi
-index+=1
-
-rows[index] = row_index
-cols[index] = N-1
-vals[index] = 1.
-index+=1
-
-row_index+=1
+	b[i] = 0. if (i==left or i==right) else dpdx/nu*h**2
 
 A = sp.csr_matrix((vals, (rows, cols)), shape=(N, N))
 
@@ -83,8 +72,9 @@ A = sp.csr_matrix((vals, (rows, cols)), shape=(N, N))
 u, _ = sla.bicgstab(A, b)
 
 plt.ioff()
-plt.plot(y, u, label='Numerical')
-plt.plot(y, uExact, label='Exact')
+plt.plot(y, u, 'r', label='Numerical')
+plt.plot(y[left:right+1], u[left:right+1], label='Numerical')
+plt.plot(y[left:right+1], uExact[left:right+1], label='Exact')
 plt.legend()
-plt.axis([-0.5,0.5,0,-dpdx/nu/8*width*width*2])
+plt.axis([-0.5,0.5,0,-dpdx/nu/8*width*width*1.5])
 plt.savefig('output.png')
